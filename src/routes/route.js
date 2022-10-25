32f6e7c3d63443ed396e801b45655f17a7f6c57f
saurabhshete100X <saurabhshete281@gmail.com>
2022-10-25 19:34:05 +0530

    Project 1 Blogging Site Completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const authorController=require('../Controller/authorController')
-const blogsController=require('../Controller/blogsController')
-const mid1=require('../MiddleWare/auth')
+const authorController = require('../Controller/authorController')
+const blogsController = require('../Controller/blogsController')
+const mid1 = require('../MiddleWare/auth')
 // const midEmail=require('../middleWare/validator')
 
-router.post('/authors',authorController.createAuthor)
+router.post('/authors', authorController.createAuthor)
 
-router.post('/login',authorController.loginAuthor)
+router.post('/login', authorController.loginAuthor)
 
-router.post('/blogs',mid1.authentication,blogsController.createBlogs)
+router.post('/blogs', mid1.authentication, blogsController.createBlogs)
 
-router.get('/blogs',mid1.authentication,blogsController.getAllBlogs)
+router.get('/blogs', mid1.authentication, blogsController.getAllBlogs)
 
-router.put('/blogs/:blogId',mid1.authentication,mid1.authorization,blogsController.updateBlog)
+router.put('/blogs/:blogId', mid1.authentication, mid1.authorization, blogsController.updateBlog)
 
-router.delete('/blogs/:blogId',mid1.authentication,mid1.authorization,blogsController.deleteBlog)
+router.delete('/blogs/:blogId', mid1.authentication, mid1.authorization, blogsController.deleteBlog)
 
 
-router.delete('/blogs',mid1.authentication,blogsController.deleteByKeys)
-
+router.delete('/blogs', mid1.authentication, blogsController.deleteByKeys)
 
 
 module.exports = router;
